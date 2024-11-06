69ee92969b2afad44864a7df22a926d54a3d637a
Tyler Eaden <tyray402@yahoo.com>
2024-11-06 09:50:08 -0700

    Initial Commit

---
+++ CheerUp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CheerUp
+//
+//  Created by Tyler Eaden on 11/6/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
